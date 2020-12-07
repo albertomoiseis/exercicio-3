2835f540e699b553d0fce453a400d1801e7c5893
albertomoiseis <75151044+albertomoiseis@users.noreply.github.com>
2020-12-07 19:39:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const $parentList = document.querySelector('.parent-list');
-
+document.body.onload = adcElmento;
 const parentList = [
   {
     name: "Marcos Maia",
@@ -21,18 +20,29 @@ const parentList = [
     name: "Lucas",
     children: "Samira",
   },
-]
+];
 
-function renderParentList(list){
-  console.log(list);
+function adcElmento() {
+  let liNovo = document.createElement("li");
+  /*
+  const conteudoNovo = document.createTextNode(
+    `${parentList[0].name} é o pai de ${parentList[0].children}`
+  );
+  */
+  const conteudoNovo = document.createTextNode(
+    parentList.map(function (valor) {
+      return `${valor.name} é o pai de ${valor.children}`;
+    })
+  );
+  liNovo.appendChild(conteudoNovo);
+  const $parentList = document.querySelector(".parent-list");
+  document.body.insertBefore(liNovo, $parentList);
 }
-
-renderParentList(parentList)
 
 //a função deve adicionar as lis dentro da ul ".parent-list", contendo o nome do pai e da criança.
 
 //ex: <li> Marcos Maia é pai de Clarice </li>
 
-// Como o array tem 5 pais, deve adicionar 5 lis de acordo com exemplo acima. 
+// Como o array tem 5 pais, deve adicionar 5 lis de acordo com exemplo acima.
 
 //dica: você pode usar o document.createElement('li') ou innerHTML;
